4e46fffb8a0094879e74c2ea1ba43f3c9dc1c09f
4ipset <129294049+4ipset111@users.noreply.github.com>
2026-05-13 22:14:57 +0400

    Update QuarkIRenderer.hpp

--- src/Renderer/QuarkIRenderer.hpp
+++ src/Renderer/QuarkIRenderer.hpp
@@ -34,8 +34,28 @@ public:
     virtual void DrawTriangle(Vec2 v1, Vec2 v2, Vec2 v3, Color color)                                               = 0;
     virtual void DrawPoly(Vec2 center, int sides, float radius, float rotation, Color color)                        = 0;
 
+
+    // Draw 3D
+    virtual void Set3DView(const Mat4& view, const Mat4& projection) = 0;
+    virtual void DrawLine3D(Vec3 startPos, Vec3 endPos, Color color) = 0;
+    virtual void DrawPlane(Vec3 center, Vec2 size, Color color) = 0;
+    virtual void DrawCube(Vec3 position, float width, float height, float length, Color color) = 0;
+    virtual void DrawCubeV(Vec3 position, Vec3 size, Color color) = 0;
+    virtual void DrawCubeWires(Vec3 position, float width, float height, float length, Color color) = 0;
+    virtual void DrawCubeWiresV(Vec3 position, Vec3 size, Color color) = 0;
+    virtual void DrawSphere(Vec3 centerPos, float radius, Color color) = 0;
+    virtual void DrawSphereEx(Vec3 centerPos, float radius, int rings, int slices, Color color) = 0;
+    virtual void DrawSphereWires(Vec3 centerPos, float radius, int rings, int slices, Color color) = 0;
+    virtual void DrawCylinder(Vec3 position, float radiusTop, float radiusBottom, float height, int slices, Color color) = 0;
+    virtual void DrawCylinderEx(Vec3 startPos, Vec3 endPos, float startRadius, float endRadius, int sides, Color color) = 0;
+    virtual void DrawCylinderWires(Vec3 position, float radiusTop, float radiusBottom, float height, int slices, Color color) = 0;
+    virtual void DrawCylinderWiresEx(Vec3 startPos, Vec3 endPos, float startRadius, float endRadius, int slices, Color color) = 0;
+    virtual void DrawGrid(int slices, float spacing) = 0;
+
     virtual void DrawText(const char* text, int x, int y, int fontSize, Color color)                                = 0;
     virtual void DrawTextEx(IFont font, const char* text, Vec2 position, float fontSize, float spacing, Color tint) = 0;
+    virtual Vec2 MeasureTextEx(IFont font, const char* text, float fontSize, float spacing)                         = 0;
+    virtual int MeasureText(const char* text, int fontSize)                                                         = 0;
 
     virtual void DrawTexture(const ITexture& texture, float x, float y, Color tint)                                             = 0;
     virtual void DrawTextureV(const ITexture& texture, Vec2 position, Color tint)                                               = 0;
@@ -49,8 +69,28 @@ public:
     virtual void BeginMode2D(const Camera2D& camera) = 0;
     virtual void EndMode2D()                         = 0;
 
+    // Camera 3D
+    virtual void BeginMode3D(const Camera3D& camera) = 0;
+    virtual void EndMode3D()                         = 0;
+
+    // Matrix stack & Transformations
+    virtual void PushMatrix() = 0;
+    virtual void PopMatrix()  = 0;
+    virtual void Translate(const Vec3& translation) = 0;
+    virtual void Rotate(float angle, const Vec3& axis) = 0;
+    virtual void Scale(const Vec3& scale) = 0;
+    virtual void MultMatrix(const Mat4& matrix) = 0;
+    virtual const float* GetMatrixModelview() = 0;
+    virtual const float* GetMatrixProjection() = 0;
+
+    virtual void EnableBackfaceCulling() = 0;
+    virtual void DisableBackfaceCulling() = 0;
+
     // Models
     virtual Model LoadModel(const char* filePath) = 0;
+    virtual void UnloadModel(Model& model)        = 0;
+    virtual void DrawModel(const Model& model, const Vec3& position, float scale, float rotationX, float rotationY, float rotationZ) = 0;
+    virtual void DrawModelEx(const Model& model, const Mat4& transform) = 0;
     
     // Texture
     virtual void BeginTextureMode(IRenderTexture target)                                                = 0;
